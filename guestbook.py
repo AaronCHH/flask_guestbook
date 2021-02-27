870b02e6f832de5ad702de53845649b99670449b
AaronCHH <aaronhsu219@gmail.com>
2021-02-28 06:28:38 +0800

    add routing

--- guestbook.py
+++ guestbook.py
@@ -8,6 +8,11 @@ def index():
   return '<h1>Hello There!</h1>'
 
 
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+  return '<h1>You are on the home page!</h1>'
+
+
 if __name__ == '__main__':
   app.config['TEMPLATES_AUTO_RELOAD'] = True
   app.jinja_env.auto_reload = True
