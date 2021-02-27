7b262bf55ab6bbd8d44a93911a0400f07ed704de
AaronCHH <aaronhsu219@gmail.com>
2021-02-28 06:47:21 +0800

    using guestbook templates

--- guestbook.py
+++ guestbook.py
@@ -5,12 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1>Hello There!</h1>'
+  return render_template('index.html')
+
+
+@app.route('/sign')
+def sign():
+  return render_template('sign.html')
 
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-  links = ['https://www.youtube.com', 'https://www.bing.com', 'https://www.python.org', 'https://www.enkato.com']
+  links = [
+      'https://www.youtube.com', 'https://www.bing.com',
+      'https://www.python.org', 'https://www.enkato.com'
+  ]
   return render_template('example.html', links=links)
 
 
