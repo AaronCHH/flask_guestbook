41000d3d07354929e227c24f1d48ecba5a4c7ce8
AaronCHH <aaronhsu219@gmail.com>
2021-02-28 08:41:47 +0800

    move model definition (model.py) to a standalone file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import model
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guestbook.db'
@@ -8,15 +9,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Comments(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(20))
-  comment = db.Column(db.String(1000))
-
-
 @app.route('/')
 def index():
-  result = Comments.query.all()
+  result = model.Comments.query.all()
   return render_template('index.html', result=result)
 
 
@@ -29,7 +24,7 @@ def sign():
 def process():
   name = request.form['name']
   comment = request.form['comment']
-  signature = Comments(name=name, comment=comment)
+  signature = model.Comments(name=name, comment=comment)
   db.session.add(signature)
   db.session.commit()
 
